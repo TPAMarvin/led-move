af818470142d5cfe56d33418e950c6c91568b58d
TPAMarvin <31587596+TPAMarvin@users.noreply.github.com>
2020-07-02 12:46:08 +0100

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,16 @@
+let strip = neopixel.create(DigitalPin.P0, 5, NeoPixelMode.RGB)
+strip.setPixelColor(0, neopixel.colors(NeoPixelColors.Red))
+let position = 0
+strip.show()
+basic.forever(function () {
+    if (position < 4) {
+        strip.rotate(1)
+        position = position + 1
+    } else {
+        strip.clear()
+        strip.setPixelColor(0, neopixel.colors(NeoPixelColors.Red))
+        position = 0
+    }
+    strip.show()
+    basic.pause(100)
+})
